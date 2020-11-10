113e8b10d3d53a1a2e17aabc61f90a9bce1f4269
Michael Linford <linford.mike@gmail.com>
2020-11-10 15:06:30 +0000

    form

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ get '/' do
   "hello!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
